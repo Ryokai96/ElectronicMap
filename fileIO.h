c5e56a171a4e8de2369396dff0733b18cad9bbce
Ryokai96 <894175329@qq.com>
2017-04-18 18:20:53 +0800

    Create fileIO.h

---
+++ fileIO.h
@@ -0,0 +1,12 @@
+#ifndef FILEIO_H
+#define FILEIO_H
+
+#include "header.h"
+#include <math.h>
+#include <string.h>
+
+void readFile(md_t n, FILE* fl);    //从文件中读取一条数据
+void writeFile(md_t n, FILE* fl);   //写入一条数据到文件中
+
+
+#endif // FILEIO_H
