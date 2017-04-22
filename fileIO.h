b928f151ec3ab16c31f0757fc43f4cb8171fc834
Ryokai96 <894175329@qq.com>
2017-04-22 11:30:02 +0800

    delete
    delete fileIO.c and fileIO.h

--- fileIO.h
+++
@@ -1,9 +0,0 @@
-#ifndef FILEIO_H
-#define FILEIO_H
-
-#include "header.h"
-
-void readFile(md_t n, FILE* fl);    //从文件中读取一条数据
-void printfFile(md_t n, FILE* fl);   //将一条数据按一定格式打印到文件中
-
-#endif // FILEIO_H
